ae1cb0dc4f5e481bd11446423254906e808d5222
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2025-05-16 22:29:14 +0700

    chore: setup initial global styles (theme, colors, typography)

---
+++ app/src/main/java/com/project/qiblaa/ui/theme/Fonts.kt
@@ -0,0 +1,28 @@
+package com.project.qiblaa.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontStyle
+import com.project.qiblaa.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_thin, FontWeight.Thin),
+    Font(R.font.poppins_extralight, FontWeight.ExtraLight),
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_extrabold, FontWeight.ExtraBold),
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_thin_italic, FontWeight.Thin, FontStyle.Italic),
+    Font(R.font.poppins_extralight_italic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.poppins_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.poppins_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.poppins_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.poppins_semibold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.poppins_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.poppins_extrabold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.poppins_black_italic, FontWeight.Black, FontStyle.Italic)
+)
