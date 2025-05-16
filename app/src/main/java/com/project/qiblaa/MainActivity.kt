099f850904b5f8357b9d8f5395bd5b4652570018
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2025-05-17 03:59:24 +0700

    feat(main): create static UI for middle section (daily pray tracker and pray time) in main page

--- app/src/main/java/com/project/qiblaa/MainActivity.kt
+++ app/src/main/java/com/project/qiblaa/MainActivity.kt
@@ -5,19 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -43,15 +52,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QiblaaTheme {
-                Scaffold(
+                Column(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(16.dp)
-                ) { innerPadding ->
+                ) {
                     UpperSection(
                         time = "Siang",
-                        modifier = Modifier.padding(innerPadding)
                     )
+                    MiddleSection()
                 }
             }
         }
@@ -59,10 +68,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun UpperSection(time: String, modifier: Modifier = Modifier) {
+fun UpperSection(
+    time: String
+) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        modifier = modifier
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         Text(
             text = "Assalamu'alaikum, Selamat $time!",
@@ -73,9 +83,9 @@ fun UpperSection(time: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun DateTimeCard(modifier: Modifier = Modifier) {
+fun DateTimeCard() {
     Column(
-        modifier = modifier
+        modifier = Modifier
             .shadow(
                 elevation = 12.dp
             )
@@ -98,8 +108,7 @@ fun DateTimeCard(modifier: Modifier = Modifier) {
                 year = 2025,
                 dayOfWeek = "Senin",
                 monthName = "Januari",
-            ),
-            modifier = Modifier
+            )
         )
         DisplayTime(
             time = Time(
@@ -109,8 +118,7 @@ fun DateTimeCard(modifier: Modifier = Modifier) {
                 amPm = "PM",
                 is24HourFormat = true,
                 timeZone = "WIB",
-            ),
-            modifier = Modifier
+            )
         )
         DisplayUpcomingEvent(
             time = Time(
@@ -118,23 +126,22 @@ fun DateTimeCard(modifier: Modifier = Modifier) {
                 minute = 30,
                 second = 0
             ),
-            event = "Asar",
-            modifier = Modifier
+            event = "Asar"
         )
     }
 }
 
 @Composable
-fun DisplayDate(date: Date, modifier: Modifier = Modifier) {
+fun DisplayDate(
+    date: Date
+) {
     Row {
         Spacer(
             modifier = Modifier
                 .weight(1f)
         )
         Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier
+            modifier = Modifier
                 .background(
                     color = Color.White,
                     shape = RoundedCornerShape(
@@ -144,7 +151,9 @@ fun DisplayDate(date: Date, modifier: Modifier = Modifier) {
                 .padding(
                     vertical = 8.dp,
                     horizontal = 12.dp
-                )
+                ),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
                 imageVector = Icons.Default.DateRange,
@@ -162,11 +171,10 @@ fun DisplayDate(date: Date, modifier: Modifier = Modifier) {
 
 @Composable
 fun DisplayTime(
-    time: Time,
-    modifier: Modifier = Modifier
+    time: Time
 ) {
     Text(
-        modifier = modifier
+        modifier = Modifier
             .padding(
                 horizontal = 24.dp
             ),
@@ -186,9 +194,12 @@ fun DisplayTime(
 }
 
 @Composable
-fun DisplayUpcomingEvent(time: Time, event: String, modifier: Modifier = Modifier) {
+fun DisplayUpcomingEvent(
+    time: Time,
+    event: String
+) {
     Text(
-        modifier = modifier
+        modifier = Modifier
             .padding(
                 start = 24.dp,
                 end = 24.dp,
@@ -210,17 +221,322 @@ fun DisplayUpcomingEvent(time: Time, event: String, modifier: Modifier = Modifie
     )
 }
 
+@Composable
+fun MiddleSection() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        Text(
+            text = "Sudahkah Anda sholat hari ini?",
+            style = MaterialTheme.typography.titleMedium,
+        )
+        DailyPrayTracker()
+        PrayTimeCard(
+            location = "Jakarta, Indonesia"
+        )
+    }
+}
+
+@Composable
+fun DailyPrayTracker() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                bottom = 12.dp
+            ),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        DailyPrayTrackerCard(
+            prayName = "Shubuh",
+            prayIcon = R.drawable.ic_shubuh,
+            isDone = true,
+            modifier = Modifier
+                .weight(1f, true)
+        )
+        DailyPrayTrackerCard(
+            prayName = "Dzuhur",
+            prayIcon = R.drawable.ic_dzuhur,
+            modifier = Modifier
+                .weight(1f, true)
+        )
+        DailyPrayTrackerCard(
+            prayName = "Ashar",
+            prayIcon = R.drawable.ic_ashar,
+            modifier = Modifier
+                .weight(1f, true)
+        )
+        DailyPrayTrackerCard(
+            prayName = "Maghrib",
+            prayIcon = R.drawable.ic_maghrib,
+            modifier = Modifier
+                .weight(1f, true)
+        )
+        DailyPrayTrackerCard(
+            prayName = "Isya",
+            prayIcon = R.drawable.ic_isya,
+            modifier = Modifier
+                .weight(1f, true)
+        )
+    }
+}
+
+@Composable
+fun DailyPrayTrackerCard(
+    modifier: Modifier = Modifier,
+    prayName: String,
+    prayIcon: Int,
+    isDone: Boolean = false
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = if (isDone) White else Primary,
+                    shape = RoundedCornerShape(
+                        size = 8.dp
+                    )
+                )
+                .border(
+                    width = 1.dp,
+                    color = Primary,
+                    shape = RoundedCornerShape(
+                        size = 8.dp
+                    )
+                )
+                .padding(
+                    vertical = 12.dp,
+                    horizontal = 8.dp
+                ),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(prayIcon),
+                contentDescription = null,
+                tint = if (isDone) Primary else White,
+            )
+            Text(
+                text = prayName,
+                style = MaterialTheme.typography.labelSmall.copy(if (isDone) Primary else White),
+            )
+        }
+        if (isDone) {
+            Icon(
+                imageVector = Icons.Default.CheckCircle,
+                contentDescription = null,
+                tint = White,
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(Alignment.BottomCenter)
+                    .offset(
+                        y = 12.dp
+                    )
+                    .background(
+                        color = Primary,
+                        shape = RoundedCornerShape(
+                            size = 12.dp
+                        )
+                    ),
+            )
+        }
+    }
+}
+
+@Composable
+fun PrayTimeCard(location: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = Primary,
+                shape = RoundedCornerShape(
+                    size = 8.dp
+                )
+            )
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = null,
+                tint = White,
+                modifier = Modifier
+                    .size(24.dp)
+            )
+            Text(
+                text = location,
+                style = MaterialTheme.typography.labelLarge.copy(color = White),
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .weight(1f)
+            )
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = null,
+                tint = White,
+                modifier = Modifier
+                    .size(24.dp)
+            )
+        }
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = White
+        )
+
+        SinglePrayTime(
+            prayName = "Shubuh",
+            prayIcon = R.drawable.ic_shubuh,
+            prayTime = Time(
+                hour = 5,
+                minute = 0,
+                second = 0,
+                timeZone = "WIB"
+            ),
+        )
+        SinglePrayTime(
+            prayName = "Terbit",
+            prayIcon = R.drawable.ic_sunrise,
+            prayTime = Time(
+                hour = 6,
+                minute = 0,
+                second = 0,
+                timeZone = "WIB"
+            ),
+            isNotification = false
+        )
+        SinglePrayTime(
+            prayName = "Dzuhur",
+            prayIcon = R.drawable.ic_dzuhur,
+            prayTime = Time(
+                hour = 12,
+                minute = 0,
+                second = 0,
+                timeZone = "WIB"
+            ),
+            isNotification = false
+        )
+        SinglePrayTime(
+            prayName = "Ashar",
+            prayIcon = R.drawable.ic_ashar,
+            prayTime = Time(
+                hour = 15,
+                minute = 0,
+                second = 0,
+                timeZone = "WIB"
+            ),
+        )
+        SinglePrayTime(
+            prayName = "Maghrib",
+            prayIcon = R.drawable.ic_maghrib,
+            prayTime = Time(
+                hour = 18,
+                minute = 0,
+                second = 0,
+                timeZone = "WIB"
+            ),
+        )
+        SinglePrayTime(
+            prayName = "Isya",
+            prayIcon = R.drawable.ic_isya,
+            prayTime = Time(
+                hour = 19,
+                minute = 0,
+                second = 0,
+                timeZone = "WIB"
+            ),
+        )
+        SinglePrayTime(
+            prayName = "Imsak",
+            prayIcon = R.drawable.ic_imsak,
+            prayTime = Time(
+                hour = 4,
+                minute = 45,
+                second = 0,
+                timeZone = "WIB"
+            ),
+        )
+    }
+}
+
+@Composable
+fun SinglePrayTime(
+    prayName: String,
+    prayIcon: Int,
+    prayTime: Time,
+    isNotification: Boolean = true,
+) {
+    val prayHour = String.format("%02d", prayTime.hour)
+    val prayMinute = String.format("%02d", prayTime.minute)
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Icon(
+            painter = painterResource(prayIcon),
+            contentDescription = null,
+            tint = White,
+            modifier = Modifier
+                .size(24.dp)
+        )
+        Text(
+            text = "${prayHour}:${prayMinute} ${prayTime.timeZone}",
+            style = MaterialTheme.typography.labelLarge.copy(color = White),
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        )
+        Text(
+            text = prayName,
+            style = MaterialTheme.typography.labelLarge.copy(color = White),
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .weight(1f)
+        )
+        if (isNotification) {
+            Icon(
+                imageVector = Icons.Outlined.Notifications,
+                contentDescription = null,
+                tint = White,
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(Alignment.CenterVertically)
+            )
+        } else {
+            Icon(
+                painter = painterResource(R.drawable.ic_notification_off),
+                contentDescription = null,
+                tint = White,
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(Alignment.CenterVertically)
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun UpperSectionPreview() {
+fun MainPagePreview() {
     QiblaaTheme {
-        Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
             UpperSection(
                 time = "Siang",
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .padding(16.dp)
             )
+            MiddleSection()
         }
     }
 }
